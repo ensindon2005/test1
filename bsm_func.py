15eb005b93aca24e9b599ccb7b64f11f66ee0ccc
ensindon2005 <edinsonrivera@gmail.com>
2018-06-24 12:51:47 +0200

    first file opt

---
+++ bsm_func.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 24 12:33:46 2018
+
+@author: edinsonrivera
+"""
+
+import pandas as pd
+import numpy as np
+import math
+
+
+class option:
+    def __init__(self, S0, t, M, K, r, sigma):
+        pass
+    
